19a94e2af94dd88677a8ec9271ebbb71616dc5f0
System Analysis and Development Student at Uninter <91498738+ApprenticeAlex@users.noreply.github.com>
2024-08-17 16:35:08 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,24 +1,28 @@
 let botao = document.querySelector("#botao");
-botao.style.background = "blue";
+botao.style.background="white";
 let estaQuebrado=false;
 let contaCliques=0;
 botao.addEventListener("mouseover",e =>{
     if(!estaQuebrado){
-        botao.style.background= "green";
+        botao.style.background="green";
         botao.style.color="white";
+    }
+}); 
+
+botao.addEventListener("mouseout",e =>{
+    if(!estaQuebrado){
+    botao.style.background="blue";
     }
 });
 
-botao.addEventListener("mouseout",e =>{
-    if(estaQuebrado){
-        botao.style.background="blue";
-}
-});
+botao.addEventListener("click",e =>{
+    
+    contaCliques++;//contaCliques=ContaCliques+1;
 
-botao.addEventListener("click", e =>{
-
-    contaCliques++;//contaCLiques=contCLiques+1;
-        botao.style.background = "red";
-        botao.innerHTML = "quebrei";
+    if(contaCliques>=10){
+        botao.style.background="red";
+        botao.innerHTML="Quebrado !";
+        botao.style.color="black";
         estaQuebrado=true;
+    }
 });
